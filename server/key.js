8e85a61dd98ab012374a15ebac575e36f375a164
siradheek <siradhee_k@hotmail.com>
2021-03-15 20:55:04 +0700

    change key

--- server/key.js
+++ server/key.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoURI : "mongodb+srv://sad7898:sad32323@cluster0.x6626.gcp.mongodb.net/forums?retryWrites=true&w=majority",
+    mongoURI : "mongodb+srv://sad7898:KzlbSMezIlt33RzF@cluster0.x6626.gcp.mongodb.net/forums?retryWrites=true&w=majority",
     secretOrKey : "ppamurof"
 }
